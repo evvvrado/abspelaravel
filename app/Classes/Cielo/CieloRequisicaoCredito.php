e8c379905bc0f74def41a09357ab7af1bc8a2531
Luis Gustavo <gusouza980@gmail.com>
2021-11-26 11:55:30 -0300

    Update

--- app/Classes/Cielo/CieloRequisicaoCredito.php
+++ app/Classes/Cielo/CieloRequisicaoCredito.php
@@ -51,7 +51,7 @@ class CieloRequisicaoCredito
     public function addPayment($valor)
     {
         $this->payment = $this->sale->payment(intval($valor * 100));
-        dd($this->payment);
+        // dd($this->payment);
     }
 
     public function addCreditCard($numero, $bandeira, $expiracao, $cvv, $nome, $parcelas)
