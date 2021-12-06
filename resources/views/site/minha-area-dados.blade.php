69e8103e584bf71835559352bef7090c14ef2429
evvvrado <everaldocrj@gmail.com>
2021-12-06 16:19:46 -0300

    update

--- resources/views/site/minha-area-dados.blade.php
+++ resources/views/site/minha-area-dados.blade.php
@@ -6,26 +6,26 @@
     <title>Área do Cliente - ABS PE</title>
 
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link rel='icon' type='image/vnd.microsoft.icon' sizes='16x16 32x32 48x48 64x64 96x96 128x128 144x144 180x180 192x192 256x256' href='{{ asset(' favicon.ico') }}' />
-    <link rel='apple-touch-icon' sizes='180x180' href='{{ asset(' site/img/logo180.png') }}'>
+    <link rel='icon' type='image/vnd.microsoft.icon' sizes='16x16 32x32 48x48 64x64 96x96 128x128 144x144 180x180 192x192 256x256' href="{{ asset('favicon.ico') }}" />
+    <link rel='apple-touch-icon' sizes='180x180' href="{{ asset(' site/img/logo180.png') }}">
     {{--
-    <link rel='manifest' href='/_ifl1/manifest.php' type='application/x-web-app-manifest+json'> --}}
-    <link rel='preload' type='text/css' as='style'
-        href='https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&family=Lato&family=Roboto:wght@500&family=Spartan:wght@400;700&display=swap' crossorigin='anonymous' />
-    <link rel='preload' type='text/css' as='style' href='{{ asset(' site/css/reset.css') }}' />
-    <link rel='preload' type='text/css' as='style' href='{{ asset(' site/css/sistema.css') }}' />
+    <link rel='manifest' href=" /_ifl1/manifest.php' type='application/x-web-app-manifest+json'> --}}
+    <link rel='preload' type='text/css' as='style' href="
+        https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&family=Lato&family=Roboto:wght@500&family=Spartan:wght@400;700&display=swap' crossorigin='anonymous' />
+    <link rel='preload' type='text/css' as='style' href=" {{ asset(' site/css/reset.css') }}" />
+    <link rel='preload' type='text/css' as='style' href="{{ asset(' site/css/sistema.css') }}" />
 
-    <link rel='preload' type='application/javascript' as='script' href='https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0='
+    <link rel='preload' type='application/javascript' as='script' href=" https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0='
         crossorigin='anonymous' />
-    <link rel='preload' type='application/javascript' as='script' href='{{ asset(' site/js/script.js') }}'>
+    <link rel='preload' type='application/javascript' as='script' href=" {{ asset(' site/js/script.js') }}">
 
-    <link rel='preload' type='text/css' as='style' href='https://use.fontawesome.com/releases/v5.15.1/css/all.css' crossorigin='anonymous' />
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.15.1/css/all.css' crossorigin='anonymous'>
+    <link rel='preload' type='text/css' as='style' href=" https://use.fontawesome.com/releases/v5.15.1/css/all.css" crossorigin=' anonymous' />
+    <link rel='stylesheet' href=" https://use.fontawesome.com/releases/v5.15.1/css/all.css" crossorigin='anonymous'>
 
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&family=Poppins&family=Lato&family=Roboto:wght@500&family=Spartan:wght@400;700&display=swap'
+    <link rel='stylesheet' href=" https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&family=Poppins&family=Lato&family=Roboto:wght@500&family=Spartan:wght@400;700&display=swap"
         crossorigin='anonymous'>
-    <link rel='stylesheet' href='{{ asset(' site/css/reset.css') }}'>
-    <link rel='stylesheet' href='{{ asset(' site/css/sistema.css') }}'>
+    <link rel='stylesheet' href=" {{ asset('site/css/reset.css') }}">
+    <link rel='stylesheet' href=" {{ asset('site/css/sistema.css') }}">
     <script src='https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=' crossorigin='anonymous'></script>
 </head>
 
