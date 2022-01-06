f473424346fcfebebbfc6e05b3d99b513afd0c12
evvvrado <everaldocrj@gmail.com>
2022-01-06 10:42:26 -0300

    update

--- app/Classes/Email.php
+++ app/Classes/Email.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Classes;
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-class Email{
-    
-    public static function enviar($file, $assunto, $destinatario, $admin = false, $attach = null, $attach_nome = "comprovante_de_compra.pdf"){
+class Email
+{
+
+    public static function enviar($file, $assunto, $destinatario, $admin = false, $attach = null, $attach_nome = "comprovante_de_compra.pdf")
+    {
         $mail = new PHPMailer(true);
 
         try {
@@ -18,20 +21,20 @@ class Email{
             $mail->Host = "email-ssl.com.br"; // Endereço do servidor SMTP
             $mail->SMTPAuth = true; // Enable SMTP authentication
             $mail->Username = 'contato@abs-pe.com.br'; // SMTP username
-            $mail->Password = 'abs@77Adm'; // SMTP password
+            $mail->Password = 'adm@77Abs'; // SMTP password
             $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 465;
 
             $mail->setFrom('contato@abs-pe.com.br', 'Contato - ABS Pernambuco');
-            if($admin){
+            if ($admin) {
                 $mail->addAddress("contato@abs-pe.com.br"); // Add a recipient, Name is optional
-            }else{
+            } else {
                 $mail->addAddress($destinatario); // Add a recipient, Name is optional
             }
             $mail->addReplyTo('contato@abs-pe.com.br', 'Contato - ABS Pernambuco');
             // print_r($_FILES['file']); exit;
 
-            if($attach){
+            if ($attach) {
                 $mail->AddAttachment($attach, $attach_nome);
             }
 
@@ -41,23 +44,22 @@ class Email{
             // $file = str_replace("{{senha}}", Str::random(6), $file);
             $mail->CharSet = 'UTF-8';
             $mail->Encoding = 'base64';
-            
+
             $mail->Subject = $assunto;
             $mail->Body    = $file;
 
             // $mail->AltBody = plain text version of your message;
 
-            if( !$mail->send() ) {
+            if (!$mail->send()) {
+
                 return false;
             } else {
+
                 return true;
             }
-
         } catch (Exception $e) {
+
             return false;
         }
     }
-
 }
-
-?>
